8407db2c4007e79a89f685425b12ecf27184b4e9
Norman <normanramadhan3@gmail.com>
2020-10-31 08:47:01 +0700

    Fix directional light value

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useSpring, a } from "react-spring/three";
 
 softShadows();
 
-const SpiningMesh = ({position, args, color, speed}) => {
+const SpinningMesh = ({ position, args, color, speed }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
@@ -26,37 +26,52 @@ const SpiningMesh = ({position, args, color, speed}) => {
   )
 }
 
-function App() { 
+function App() {
   return (
     <>
-     <Canvas shadowMap colorManagement camera={{position: [-5, 2, 10], fov: 60}} >
-       <ambientLight intensity={1} />
-       <directionalLight 
-        castShadow
-        position={[0, -10, 0]} 
-        intensity={1.5} 
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={50}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-      />
-       <pointLight position={[-10, 0, -20]} intensity={0.5} />
-       <pointLight position={[0, -10, 0]} intensity={1.5} />
-
-      <group >
-        <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]} >
-          <planeBufferGeometry attach="geometry" args={[100, 100]} />
-          <shadowMaterial attach="material" opacity={1} />
-        </mesh>
-        <SpiningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="lightblue" speed={2}/>
-        <SpiningMesh position={[-2, 1, -5]} color="pink" speed={6}/>
-        <SpiningMesh position={[5, 1, -2]} color="pink" speed={6}/>
-      </group> 
-       <OrbitControls />
-     </Canvas>
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{ position: [-5, 2, 10], fov: 60 }}>
+        {/* This light makes things look pretty */}
+        <ambientLight intensity={0.3} />
+        {/* Our main source of light, also casting our shadow */}
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        {/* A light to help illumnate the spinning boxes */}
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <group>
+          {/* This mesh is the plane (The floor) */}
+          <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+            receiveShadow>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <shadowMaterial attach='material' opacity={0.3} />
+          </mesh>
+          <SpinningMesh
+            position={[0, 1, 0]}
+            color='lightblue'
+            args={[3, 2, 1]}
+            speed={2}
+          />
+          <SpinningMesh position={[-2, 1, -5]} color='pink' speed={6} />
+          <SpinningMesh position={[5, 1, -2]} color='pink' speed={6} />
+        </group>
+        {/* Allows us to move the canvas around for different prespectives */}
+        <OrbitControls />
+      </Canvas>
     </>
   );
 }
